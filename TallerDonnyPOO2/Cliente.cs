dd468570ce33ceeae5a87b71121b458c05c8a1a3
andres Londoño <andresmedellin215@gmail.com>
2022-02-28 12:14:02 -0500

    Agregar archivos de proyecto.

---
+++ TallerDonnyPOO2/Cliente.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TallerDonnyPOO2
+{
+    internal class Cliente
+    {
+        string Nombre;
+        string Documento;
+        string Direccion;
+        int Telefono;
+
+        public void Setnombre(string nombre)
+        {
+            Nombre = nombre;
+        }
+        public void Setdocumento(string documento)
+        {
+            Documento = documento;
+        }
+        public void Setdireccion(string direccion)
+        {
+            Direccion = direccion;
+        }
+        public void Setdtelefono(int telefono)
+        {
+            Telefono = telefono;
+        }
+
+        public string Getnombre()
+        {
+            return Nombre;
+        }
+        public string Getdocumento()
+        {
+            return Documento;
+        }
+        public string Getdireccion()
+        {
+            return Direccion;
+        }
+        public int Gettelefono()
+        {
+            return Telefono;
+        }
+
+
+        static void ValidarDocumento(string documento)
+        {
+
+        }
+
+        public void Crearcliente()
+        {
+            //ACA SE COMIENZAN A LLENAR LOS DATOS DEL OBJETO.
+
+            Console.WriteLine("Ingrese el nombre");
+            Setnombre(Console.ReadLine());
+            Console.WriteLine("Ingrese la direccion");
+            Setdireccion(Console.ReadLine());
+            Console.WriteLine("Ingrese el telefono");
+            Setdtelefono(int.Parse(Console.ReadLine()));
+            Console.WriteLine("Ingrese el numero de la Cedula ");
+            Setdocumento(Console.ReadLine());
+        }
+
+
+
+        public void Menu() //ACA SE CREA EL MENU QUE LE DA LA OPCION AL USUARIO DE LO QUE DESEA HACER .
+        {
+            Console.WriteLine("\nSeleccione una opcion");
+            Console.WriteLine("\n1. Crear  Cliente");
+            Console.WriteLine("\n2. Buscar Cliente");
+            Console.WriteLine("\n3. Modificar Cliente");
+            Console.WriteLine("\n4. Eliminar Cliente");
+            Console.WriteLine("\n5.Salir");
+
+        }
+        public int Buscarcliente(int documento)
+        {
+
+            return 0;
+        }
+        public void Modificarcliente()
+        {
+
+        }
+        public void Eliminarcliente()
+        {
+
+        }
+    }
+
+
+
+}
---
+++ TallerDonnyPOO2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace TallerDonnyPOO2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int respuesta;
+
+            Cliente clienteuno = new();//instanciar
+
+
+            //ACA CREAMOS EL MENU QUE SEA UTILIZABLE VARIAS VECES
+
+            clienteuno.Menu();
+            do
+            {
+               
+                respuesta = int.Parse(Console.ReadLine());
+                switch (respuesta)
+                {
+                    case 1:
+                        clienteuno.Crearcliente();
+                      break;
+                    case 2:
+                      //  clienteuno.Buscarcliente();
+                        break;
+                    case 3:
+                        clienteuno.Modificarcliente();
+                        break;
+                    case 4:
+                        clienteuno.Eliminarcliente();
+                        break;
+                    case 5:
+                        Console.WriteLine("SE SALIO DEL PROGRAMA EXITOSAMENTE");
+                        break;
+
+
+                }
+                Console.WriteLine("\nSeleccione otra opcion");
+            } while (respuesta !=5);
+           
+
+        
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
