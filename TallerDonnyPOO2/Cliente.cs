fdf1453d62d8e9257ecb6a77a5e281fbc707d59e
andres Londoño <andresmedellin215@gmail.com>
2022-03-02 20:33:09 -0500

    ok

--- TallerDonnyPOO2/Cliente.cs
+++ TallerDonnyPOO2/Cliente.cs
@@ -9,66 +9,6 @@ namespace TallerDonnyPOO2
 {
     internal class Cliente
     {
-      public  string Nombre;
-        public string Documento;
-        public string Direccion;
-        public int Telefono;
-        public string validarDocumento;
-
-        /*
-         
-        public void Setnombre(string nombre)
-        {
-            Nombre = nombre;
-        }
-        public void Setdocumento(string documento)
-        {
-            Documento = documento;
-        }
-        public void Setdireccion(string direccion)
-        {
-            Direccion = direccion;
-        }
-        public void Setdtelefono(int telefono)
-        {
-            Telefono = telefono;
-        }
-
-        public string Getnombre()
-        {
-            return Nombre;
-        }
-        public string Getdocumento()
-        {
-            return Documento;
-        }
-        public string Getdireccion()
-        {
-            return Direccion;
-        }
-        public int Gettelefono()
-        {
-            return Telefono;
-        }
-        */
-
-
-
-
-        public void Menu() //ACA SE CREA EL MENU QUE LE DA LA OPCION AL USUARIO DE LO QUE DESEA HACER .
-        {
-            Console.WriteLine("\nSeleccione una opcion");
-            Console.WriteLine("\n1. Crear  Cliente");
-            Console.WriteLine("\n2. Buscar Cliente");
-            Console.WriteLine("\n3. Modificar Cliente");
-            Console.WriteLine("\n4. Eliminar Cliente");
-            Console.WriteLine("\n5.Salir");
-
-        }
-
-        
-
-    }
 
 
 
--- TallerDonnyPOO2/Program.cs
+++ TallerDonnyPOO2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace TallerDonnyPOO2
 {
@@ -8,72 +9,7 @@ namespace TallerDonnyPOO2
         static void Main(string[] args)
 
         {
-            int respuesta;
-           
-            Cliente clienteuno = new();//instanciar
-            List<Cliente> listaCliente = new();//se crea la lista de tipo clase
-
-
-            //ACA CREAMOS EL MENU QUE SEA UTILIZABLE VARIAS VECES
-
-            clienteuno.Menu();
-            do
-            {
-               
-                respuesta = int.Parse(Console.ReadLine());
-                switch (respuesta)
-                {
-                    case 1:
-                        //se crea el cliente
-                        Console.WriteLine("Ingrese el nombre de la persona");
-                        clienteuno.Nombre = Console.ReadLine();
-
-                        Console.WriteLine("Ingrese direccion");
-                        clienteuno.Direccion = Console.ReadLine();
-
-                        Console.WriteLine("Ingrese telefono");
-                        clienteuno.Telefono =int.Parse( Console.ReadLine());
-
-                        Console.WriteLine("ingrese numero del documento");
-                        clienteuno.Documento = Console.ReadLine();
-                        listaCliente.Add(clienteuno);//aca agrega a la lista un objeto tipo cliente
-                        
-
-                      break;
-                    case 2:
-                        //se busca el cliente
-                        Console.WriteLine("ingrese el documento del cliente");
-                        clienteuno.validarDocumento = Console.ReadLine();
-
-                        if (clienteuno.validarDocumento==clienteuno.Documento)
-                        {
-                            Console.WriteLine("El  nombre es : "+clienteuno.Nombre);
-                            Console.WriteLine("La Direccion : "+clienteuno.Direccion);
-                            Console.WriteLine("Telefono :"+clienteuno.Telefono);
-
-                        }
-                     
-                        break;
-                    case 3:
-                   
-                        break;
-                    case 4:
-                       
-                        break;
-                    case 5:
-                        Console.WriteLine("SE SALIO DEL PROGRAMA EXITOSAMENTE");
-                        break;
-
-
-                }
-                Console.WriteLine("\nSeleccione otra opcion");
-            } while (respuesta !=5);
-           
-
-        
-            Console.ReadKey();
-
-
+            
 
         }
     }
